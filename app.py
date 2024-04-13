adc968f87d67ffc4f88b4fce9a5d47e696c691ab
Rohan <rohanjosepharun@gmail.com>
2024-04-13 22:38:12 +0530

    added to boxes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect
 #from flask_ckeditor import CKEditor
 import json
 
@@ -7,18 +7,32 @@ app= Flask(__name__)
 #app.config['CKEDITOR_PKG_TYPE'] = 'standard'
 #ckeditor = CKEditor(app)
 
+
+
 @app.route("/")
+
+
 def index():
-    return render_template("index.html")
+    suggestion_list=["hi berooo","how areeee you"]
+    list_count = len(suggestion_list)    
+    return render_template("index.html",suggestion_list=suggestion_list,list_count=list_count)
 
 
 @app.route('/test', methods=['POST'])
 def test():
     output = request.get_json()
     #print(type(output["ops"]))
+    
+    
+
+    output_file = open("output.txt", "w")
     for i in output["ops"]:
-        print(i["insert"])
-    return output
+
+        output_file.write(i["insert"])
+
+    output_file.close()
+    print("hiii")
+    return redirect("/helloworld")
 
 
 if __name__ == "__main__":
